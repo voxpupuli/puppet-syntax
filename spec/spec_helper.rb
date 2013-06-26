df99aa8fb2d2fa6730779a8f88bd6d1329791e52
Dan Carley <dan.carley@gmail.com>
2013-06-26 21:59:42 +0100

    Add a helper method for loading fixtures
    I'm not very happy with this approach. I'd prefer to have the Puppet and ERB
    code inline with the `it{}` blocks and written to temporary files. However
    this is a suitable MVP.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,11 @@
 require 'rspec'
 require 'puppet-syntax'
 
+def fixture_files(list)
+  list = [list].flatten
+  list.map { |f| File.expand_path("../fixtures/#{f}", __FILE__) }
+end
+
 RSpec.configure do |config|
   config.color_enabled = true
   config.formatter     = 'documentation'
