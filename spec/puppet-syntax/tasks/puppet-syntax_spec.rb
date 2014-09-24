aba1c8b6c11e04f66501a6c0bab2312df29f315b
Martin Alfke <martin.alfke@buero20.org>
2014-09-24 15:01:02 -0700

    RSpec 2 and 3 compatibility

--- spec/puppet-syntax/tasks/puppet-syntax_spec.rb
+++ spec/puppet-syntax/tasks/puppet-syntax_spec.rb
@@ -2,10 +2,18 @@ require 'spec_helper'
 
 describe 'PuppetSyntax rake tasks' do
   it 'should generate FileList of manifests relative to Rakefile' do
-    pending
+    if RSpec::Version::STRING < '3'
+      pending
+    else
+      skip('needs to be done')
+    end
   end
 
   it 'should generate FileList of templates relative to Rakefile' do
-    pending
+    if RSpec::Version::STRING < '3'
+      pending
+    else
+      skip('needs to be done')
+    end
   end
 end
--- spec/puppet-syntax/templates_spec.rb
+++ spec/puppet-syntax/templates_spec.rb
@@ -11,52 +11,52 @@ describe PuppetSyntax::Templates do
     files = fixture_templates('pass.erb')
     res = subject.check(files)
 
-    res.should == []
+    expect(res).to match([])
   end
 
   it 'should ignore NameErrors from unbound variables' do
     files = fixture_templates('pass_unbound_var.erb')
     res = subject.check(files)
 
-    res.should == []
+    expect(res).to match([])
   end
 
   it 'should catch SyntaxError' do
     files = fixture_templates('fail_error.erb')
     res = subject.check(files)
 
-    res.should have(1).items
-    res.first.should match(/2: syntax error, unexpected/)
+    expect(res.size).to eq(1)
+    expect(res[0]).to match(/2: syntax error, unexpected/)
   end
 
   it 'should catch Ruby warnings' do
     files = fixture_templates('fail_warning.erb')
     res = subject.check(files)
 
-    res.should have(1).items
-    res.first.should match(/2: warning: found = in conditional/)
+    expect(res.size).to eq(1)
+    expect(res[0]).to match(/2: warning: found = in conditional/)
   end
 
   it 'should read more than one valid file' do
     files = fixture_templates(['pass.erb', 'pass_unbound_var.erb'])
     res = subject.check(files)
 
-    res.should == []
+    expect(res).to match([])
   end
 
   it 'should continue after finding an error in the first file' do
     files = fixture_templates(['fail_error.erb', 'fail_warning.erb'])
     res = subject.check(files)
 
-    res.should have(2).items
-    res[0].should match(/2: syntax error, unexpected/)
-    res[1].should match(/2: warning: found = in conditional/)
+    expect(res.size).to eq(2)
+    expect(res[0]).to match(/2: syntax error, unexpected/)
+    expect(res[1]).to match(/2: warning: found = in conditional/)
   end
 
   it 'should ignore a TypeError' do
     files = fixture_templates('typeerror_shouldwin.erb')
     res = subject.check(files)
 
-    res.should == []
+    expect(res).to match([])
   end
 end
